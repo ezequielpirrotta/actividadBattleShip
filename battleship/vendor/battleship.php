ac8622c058055719f0164fc9469ba79bd621f094
ezequielpirrotta <ezequiel.pir@outlook.com>
2019-12-21 15:02:24 -0300

    primero

---
+++ battleship/vendor/battleship.php
@@ -0,0 +1,112 @@
+<?php
+class Battleship{
+    
+    private $raws;
+    private $cols;
+    private $ships;
+    private $jugador1;
+    private $jugador2;
+    private $count1;
+    private $count2;
+    private $turnsQuantity;
+    private $turn;
+    public function __construct($raws,$cols,$ships){
+        $this->count1=0;
+        $this->count2=0;
+        $this->turnsQuantity=0;
+        $this->turn=1;
+        $this->raws=$raws;
+        $this->cols=$cols;
+        $this->ships=$ships;
+        $i=0;
+        while($i<$raws){
+            $this->jugador1[$i]=array();
+            $j=0;
+            while($j<$cols){
+                $this->jugador1[$i][$j]=0;
+                $j+=1;
+            }
+            $i+=1;
+        }
+        $i=0;
+        while($i<$raws){
+            $this->jugador2[$i]=array();
+            $j=0;
+            while($j<$cols){
+                $this->jugador2[$i][$j]=0;
+                $j++;
+            }
+            $i++;
+        }
+    }
+    function mostrarCantBarcos(){
+        return $this->ships;
+    }
+    function mostrarJugador1(){
+        return $this->jugador1;
+    }
+    function mostrarJugador2(){
+        return $this->jugador2;
+    }
+    function colocarNaveJugador1($raw,$col){
+        if($this->count1<$this->ships and $this->jugador1[$raw][$col]===0){
+           $this->jugador1[$raw][$col]=1;
+           $this->count1; 
+        }
+    }
+    function colocarNaveJugador2($raw,$col){
+        if($this->count2<$this->ships and $this->jugador2[$raw][$col]===0){
+            $this->jugador2[$raw][$col]=1;
+            $this->count2+=1;
+        }
+    }
+    function turnoJugador1($raw,$col){
+        
+        if($this->jugador2[$raw][$col]==1 and $this->turn==1){
+            $this->jugador2[$raw][$col]=2;
+            $this->turn=2;
+        }
+        $this->turnsQuantity+=1;
+    }
+    function turnoJugador2($raw,$col){
+        
+        if($this->jugador1[$raw][$col]==1 and $this->turn==2){
+            $this->jugador1[$raw][$col]=2;
+            $this->turn=1;
+        }
+        $this->turnsQuantity+=1;
+    }
+    function ganaJugador1(){
+        for($i=0;$i<$this->raws;$i+=1){
+            for($j=0;$j<$this->cols;$j+=1){
+                if($this->jugador2[$i][$j]==1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    function ganaJugador2(){
+        for($i=0;$i<$this->raws;$i+=1){
+            for($j=0;$j<$this->cols;$j+=1){
+                if($this->jugador1[$i][$j]==1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    function terminoElJuego(){
+        $aux1=$this->ganaJugador1();
+        $aux2=$this->ganaJugador2();
+        if($aux1 or $aux2){
+            return true;
+        }
+        return false;
+    }
+    function cuantosTurnosPasaron(){
+        return $this->turnsQuantity;
+    }
+
+
+}
---
+++ battleship/vendor/battleship_test.php
@@ -0,0 +1,89 @@
+<?php
+require_once("./autoload.php");
+include ("battleship.php");
+use PHPUnit\Framework\TestCase;
+final class AhorcadoTest extends TestCase
+{
+    function testExisteClaseBattleship(){
+        
+        $this->assertTrue(class_exists("battleship"));
+    }
+    function testNoEstaVacio(){
+        $batalla=new Battleship(20,20,10);
+        $this->assertTrue(!empty($batalla));
+    }
+    function testBarcos(){
+        $batalla=new Battleship(20,20,10);
+        $this->assertEquals(10,$batalla->mostrarCantBarcos());
+    }
+    function testMostrarJugador1(){
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador1(10,10);
+        $aux=$batalla->mostrarJugador1();
+        $this->assertEquals(1,$aux[10][10]);
+    }
+    function testColocar11BarcosJugador2(){
+        $contador=0;
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador2(10,10);
+        $batalla->colocarNaveJugador2(3,16);
+        $batalla->colocarNaveJugador2(4,16);
+        $batalla->colocarNaveJugador2(5,16);
+        $batalla->colocarNaveJugador2(6,16);
+        $batalla->colocarNaveJugador2(7,16);
+        $batalla->colocarNaveJugador2(8,16);
+        $batalla->colocarNaveJugador2(9,16);
+        $batalla->colocarNaveJugador2(10,17);    
+        $batalla->colocarNaveJugador2(11,13);
+        $batalla->colocarNaveJugador2(12,13);
+        for($i=0;$i<20;$i++){
+            for($j=0;$j<20;$j++){
+                $aux=$batalla->mostrarJugador2();
+                if($aux[$i][$j]==1){
+                    $contador++;
+                }
+            }
+        }
+        $this->assertEquals(10,$contador);
+    }
+    function testTurnoJugador1(){
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador2(10,10);
+        $batalla->turnoJugador1(10,10);
+        $this->assertEquals(2,$batalla->mostrarJugador2()[10][10]);
+        
+    }
+    function testGanaJugador1(){
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador2(10,10);
+        $batalla->colocarNaveJugador1(9,9);
+        $batalla->turnoJugador1(10,10);
+        $this->assertTrue($batalla->ganaJugador1());
+        
+    }
+    function testTerminaElJuego(){
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador2(10,10);
+        $batalla->colocarNaveJugador1(9,9);
+        $batalla->turnoJugador1(10,10);
+        $this->assertTrue($batalla->terminoElJuego());
+    }
+    function testJuegarMasDeUnaVez(){
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador2(10,10);
+        $batalla->colocarNaveJugador2(5,5);
+        $batalla->colocarNaveJugador1(9,9);
+        $batalla->turnoJugador1(10,10);
+        $batalla->turnoJugador1(5,5);
+        $this->assertFalse($batalla->terminoElJuego());
+    }
+    function testCantidadDeTurnos(){
+        $batalla=new Battleship(20,20,10);
+        $batalla->colocarNaveJugador2(10,10);
+        $batalla->colocarNaveJugador2(5,5);
+        $batalla->colocarNaveJugador1(9,9);
+        $batalla->turnoJugador1(10,10);
+        $batalla->turnoJugador2(9,9);
+        $this->assertEquals(2,$batalla->cuantosTurnosPasaron());
+    }
+}
